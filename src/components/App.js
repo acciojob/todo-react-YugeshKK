4eb2236a368f4d03bde4f4adbac8760fdc17226f
YKK <yugeshkk2001@gmail.com>
2023-11-27 19:44:51 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
 
-import React from "react";
+import React, { useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
+  const addTodo = () => {
+    if (newTodo.trim() !== '') {
+      setTodos([...todos, { id: Date.now(), text: newTodo, completed: false }]);
+      setNewTodo('');
+    }
+  };
+
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className='app'>
+      <h1>Todo App</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <p key={todo.id}>
+            <span>
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </p>
+        ))}
+      </ul>
+    </div>
+  );
+};
 export default App
